37e9cd080d31fe4afe1bac0023ca0ffa5340fcc5
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-11-09 14:00:15 +0200

    interactive and non-interactive modes

---
+++ headers.h
@@ -0,0 +1,6 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+char *readline();
+
+#endif /* HEADERS_H */
---
+++ readline.c
@@ -0,0 +1,23 @@
+#include "headers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *readline()
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+
+	nread = getline(&line, &len, stdin);
+	if (nread == -1) {
+		// Error or end of file
+		free(line);
+		return NULL;
+	}
+
+	// Remove the trailing newline character, if it exists
+	if (line[nread - 1] == '\n')
+		line[nread - 1] = '\0';
+
+	return line;
+}
