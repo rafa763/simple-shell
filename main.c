052f0a4672f3f9aa50f326a191c45bd29d565fc7
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-11-10 19:25:26 +0200

    handle and process command in any mode

---
+++ main.c
@@ -0,0 +1,43 @@
+#include "headers.h"
+
+/**
+ * main - Entry point for the shell, handles the interactive and non-interactive
+ * modes of the shell
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return - 0 on success, -1 of failure
+ */
+int main(int ac, char **av)
+{
+	char *input;
+
+	// get the input from the user
+	if (isatty(STDIN_FILENO))
+	{
+		// interactive
+		while (1)
+		{
+			printf("$ ");
+			input = readline();
+			if (input == NULL)
+				return (0);
+			process(input);
+			//printf("%s\n", input);
+			free(input);
+		}
+	}
+	else
+	{
+		// non interactive
+		input = readline();
+		if (input == NULL)
+		    return (0);
+		process(input);
+		//printf("%s\n", input);
+		free(input);
+	}
+
+	return (0);
+}
