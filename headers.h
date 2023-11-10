de5dd9aa28e5770dd4c7bfc9e2fe1b5bf4715775
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-11-10 19:10:09 +0200

    add prototypes and unify lib declarations

--- headers.h
+++ headers.h
@@ -1,6 +1,20 @@
 #ifndef HEADERS_H
 #define HEADERS_H
 
+
+/* Importing Libraries */
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+
+/* Function Prototypes */
 char *readline();
+int process(char *input);
+int checkcommand(char *command, char **args);
+char *getpath(char *command);
 
 #endif /* HEADERS_H */
