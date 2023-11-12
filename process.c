cdad21343f0a22063d24997a1f952f725d76911e
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-11-13 01:27:41 +0200

    add support for && || ; and comments

--- process.c
+++ process.c
@@ -29,3 +29,61 @@ int process(char *input)
 
 	return (0);
 }
+
+int parse(char *input)
+{
+	char *p1, *p2, *p3, buffer[100];  // Adjust the size according to your needs
+
+	p1 = input;
+	p2 = buffer;
+	p3 = p2;
+
+	while (*p1 != '\0')
+	{
+		if (*p1 == '&' && *(p1 + 1) == '&')
+		{
+			*p2 = '\0';
+			process(p3);
+			p1 += 2;  // Skip the '&&'
+			p2 = buffer;
+			p3 = p2;
+		}
+		else if (*p1 == '|' && *(p1 + 1) == '|')
+		{
+			*p2 = '\0';
+			process(p3);
+			//printf("%s\n", p3);
+			p1 += 2;  // Skip the '||'
+			p2 = buffer;
+			p3 = p2;
+		}
+		else if (*p1 == ';')
+		{
+			*p2 = '\0';
+			process(p3);
+			//printf("%s\n", p3);
+			p1++;
+			p2 = buffer;
+			p3 = p2;
+		}
+		else if (*p1 == '#' && *(p1 - 1) == ' ')
+		{
+			*p2 = '\0';
+			//printf("%s\n", p3);
+			process(p3);
+			p3 = p2;
+			return (0);
+		}
+		else
+		{
+			*p2++ = *p1++;
+		}
+	}
+
+	*p2 = '\0';  // Null-terminate the modified string
+
+	//printf("%s\n", p3);
+	process(p3);
+
+	return 0;
+}
