6fa515e6d0d00e6aa0b7bd39b1a86565a7a1ec6f
rahma korish <rahmakorish@gmail.com>
2023-11-13 09:06:55 -0800

    change exit argument to int

--- exit.c
+++ exit.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
-#include "headers.h"
+#include <stdlib.h>
 /*
  * _exitshell:exit the shell
  * status:user input to exit command
@@ -25,3 +25,42 @@ int _exitshell(int status)
     exit(EXIT_FAILURE);}
  /* exit with user input*/
   _exitshell(userinput);
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+int string2num(char *str)
+{
+    int signal = 1;
+    int i;
+    unsigned int num = 0,flag = 0,result = 0;
+
+    for (i = 0;  str[i] != '\0' && flag != 2; i++)
+    {
+        if (str[i] == '-')
+            signal *= -1;
+
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            flag = 1;
+            num *= 10;
+            num += (str[i] - '0');
+        }
+        else if (flag == 1)
+            flag = 2;
+    }
+
+    if (signal == -1)
+        result = -num;
+    else
+        result = num;
+
+    return (result);
+}
+int main(void)
+{ 
+  int num = string2num("-332");
+  printf("%d\n",num);
+}
