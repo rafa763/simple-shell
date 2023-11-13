0d5bd0a653ff616431a8cc56319452640d5a895c
rahma korish <rahmakorish@gmail.com>
2023-11-13 08:45:40 -0800

    updates

--- exit.c
+++ exit.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include "headers.h"
 /*
  * _exitshell:exit the shell
  * status:user input to exit command
@@ -7,23 +8,20 @@
 int _exitshell(int status)
 {
   if (status == 0 || !status)
-  {printf("\nnormal exit\n");
-  _exit(0);}
+  exit(0);
 
   else if (status > 0)
-  {printf("\n exit\n");
-  _exit(status);}
-  
+  exit(status);
+
   if (status < 0)
- { printf("\nerror exit\n");
-  _exit(status);
-}
+  exit(status);
 }
 
-int main(void)
-{
-//_exitshell(3);
-//_exitshell(0);
-//_exitshell(-1);
-_exitshell('\0');
-}
+/* exit with success*/
+  void _successexit(void) {
+    exit(EXIT_SUCCESS);}
+ /* exit with failure*/
+ void _failexit(void) {
+    exit(EXIT_FAILURE);}
+ /* exit with user input*/
+  _exitshell(userinput);
