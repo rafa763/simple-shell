8ba6bbb5a27ca10c2da9b6566130e7fa2f18919c
rahma korish <rahmakorish@gmail.com>
2023-11-14 14:02:20 -0800

    memory leak fix

---
+++ newpath.c
@@ -0,0 +1,41 @@
+#include <headers.h>
+char *getpath(char *command)
+{
+    int i, stat;
+    char *token, *path = "", full[1024];
+    extern char **environ;
+    char *sign = "/";
+
+    stat = access(command, X_OK);
+    if (stat == 0)
+        return (command);
+
+    for (i = 0; environ[i]; i++)
+    {
+        token = strtok((environ[i]), "=");
+
+        if (strcmp("PATH", token) == 0)
+        {
+            token = strtok(NULL, "=");
+            break;
+        }
+    }
+
+
+    token = strtok(token, ":");
+    while (token)
+    {
+        strcpy(full,token);
+        strcat(full,sign);
+        strcat(full,command);
+//	printf("full: %s\n",full);
+	path= strdup(full);
+//	printf("path: %s\n",path);
+        stat = access(path, X_OK);
+        if (stat == 0)
+            return (path);
+        token = strtok(NULL, ":");
+	free(path);
+    }
+    return (NULL);
+}
