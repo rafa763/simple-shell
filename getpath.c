aabda0a70839aad979e0c51c64fb1125ff84a309
rahma korish <rahmakorish@gmail.com>
2023-11-15 16:31:14 -0800

    betty style to path

--- getpath.c
+++ getpath.c
@@ -1,41 +1,42 @@
 #include "headers.h"
+/**
+ *getpath-checks for executable file in enviroment variable PATH
+ *@command:user input to commandline
+ *Return:executable path
+ **/
 char *getpath(char *command)
 {
-    int i, stat;
-    char *token, *path = "", full[1024];
-    extern char **environ;
-    char *sign = "/";
+	int i, stat;
+	char *token, *path = "", full[1024];
+	char *sign = "/";
 
-    stat = access(command, X_OK);
-    if (stat == 0)
-        return (command);
+	stat = access(command, X_OK);
+	if (stat == 0)
+		return (command);
 
-    for (i = 0; environ[i]; i++)
-    {
-        token = strtok(strdup(environ[i]), "=");
-
-        if (strcmp("PATH", token) == 0)
-        {
-            token = strtok(NULL, "=");
-            break;
-        }
-    }
-
-
-    token = strtok(token, ":");
-    while (token)
-    {
-        strcpy(full,token);
-        strcat(full,sign);
-        strcat(full,command);
-	path= strdup(full);
-        stat = access(path, X_OK);
-        if (stat == 0)
+	for (i = 0; environ[i]; i++)
 	{
-            return(path);
+		token = strtok(strdup(environ[i]), "=");
+		if (strcmp("PATH", token) == 0)
+		{
+			token = strtok(NULL, "=");
+			break;
+		}
 	}
-        token = strtok(NULL, ":");
-	free(path);
-    }
-    return (NULL);
+	token = strtok(token, ":");
+	while (token)
+	{
+		strcpy(full, token);
+		strcat(full, sign);
+		strcat(full, command);
+		path = strdup(full);
+		stat = access(path, X_OK);
+		if (stat == 0)
+		{
+			return (path);
+		}
+		token = strtok(NULL, ":");
+		free(path);
+	}
+	return (NULL);
 }
